ac7785a8262aec2165263898ecfcb12a65b6b990
JackA1ltman <cs2dtzq@163.com>
2026-06-02 15:00:04 +0800

    treewide: bpf: Checkout to 5.10
    tiffanyy:
    * Continuing to port back to k4.4 on the basis of porting to k4.9, the
      current xsk BPF tram line BPF dispatcher is temporarily unavailable
      due to incomplete porting. Therefore, they are disabled
    
    From:
    https://github.com/jsdizkcksv/psionic_kernel_xiaomi_mido
    
    Change-Id: I16e84be7a8fb57a4d304e3ca709047162e36650e
    Signed-off-by: tiffanyy <yangfanyan7@gmail.com>
    Signed-off-by: JackA1ltman <cs2dtzq@163.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1151,7 +1151,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
-		strcpy(tmp.release, "5.4.186");
+		strcpy(tmp.release, "5.10.199");
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
@@ -1425,7 +1425,8 @@ int do_prlimit(struct task_struct *tsk, unsigned int resource,
 	 * applications, so we live with it
 	 */
 	 if (!retval && new_rlim && resource == RLIMIT_CPU &&
-			 new_rlim->rlim_cur != RLIM_INFINITY)
+	     new_rlim->rlim_cur != RLIM_INFINITY &&
+	     IS_ENABLED(CONFIG_POSIX_TIMERS))
 		update_rlimit_cpu(tsk, new_rlim->rlim_cur);
 out:
 	read_unlock(&tasklist_lock);
