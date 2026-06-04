e3727a7e5f9348b33197b050726bc834ec4c6dae
juezhi6668 <1649417471@qq.com>
2026-06-04 14:08:29 +0800

    Revert "f2fs: show the CP_PAUSE reason in checkpoint traces"
    This reverts commit dd4c536a8c74d2a2b6c56ab2b3e3a970fd7bce10.

--- include/trace/events/f2fs.h
+++ include/trace/events/f2fs.h
@@ -45,7 +45,6 @@ TRACE_DEFINE_ENUM(CP_SYNC);
 TRACE_DEFINE_ENUM(CP_RECOVERY);
 TRACE_DEFINE_ENUM(CP_DISCARD);
 TRACE_DEFINE_ENUM(CP_TRIMMED);
-TRACE_DEFINE_ENUM(CP_PAUSE);
 
 #define show_block_type(type)						\
 	__print_symbolic(type,						\
@@ -122,12 +121,11 @@ TRACE_DEFINE_ENUM(CP_PAUSE);
 
 #define show_cpreason(type)						\
 	__print_symbolic(type,						\
-		{ CP_UMOUNT,    "Umount" },                             \
+		{ CP_UMOUNT,	"Umount" },				\
 		{ CP_FASTBOOT,	"Fastboot" },				\
 		{ CP_SYNC,	"Sync" },				\
 		{ CP_RECOVERY,	"Recovery" },				\
 		{ CP_DISCARD,	"Discard" },				\
-		{ CP_PAUSE,	"Pause" },				\
 		{ CP_UMOUNT | CP_TRIMMED,	"Umount,Trimmed" })
 
 #define show_fsync_cpreason(type)					\
